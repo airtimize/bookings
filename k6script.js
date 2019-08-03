e1aedf702bc2f225865500e87a00977e6e14ac5b
BrandonSYu <beyu94@gmail.com>
2019-08-03 12:57:45 -0700

    k6 script changes

--- k6script.js
+++ k6script.js
@@ -7,27 +7,22 @@ export let options = {
   // simulate rampup of traffic from 1 to 200 users over 5 minutes.
   stages: [
     { duration : "1m", target : 200},
-    { duration : "2m", target: 4000},
-    { duration : "1m", target : 5500},
-    { duration : "2m", target : 3000},
-    { duration : "1m", target : 1000}
+    { duration : "2m", target : 5500},
+    { duration : "1m", target: 4000},
   ]
 };
 
 let errorRate = new Rate("API errors");
-
 let getUser = function(){
     let min = 1;
     let max = 9999999;
     var rand = Math.floor(Math.random() * (max - min)) + min;
-    let baseUrl = 'http://localhost:3001';
-  let listingRes = http.get(`${baseUrl}/rooms/${rand}/`, {}, {
+    let listingRes = http.get(`http://localhost:3001/rooms/${rand}/`, {}, {
   });
 
   check(listingRes, {
       "Listing retrieval successful": (r) => r.status === 200
   }) || errorRate.add(1);
-
   return listingRes;
 };
 
