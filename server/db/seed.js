3c072b12f543b1fa33415dca279c7af94df39cab
BrandonSYu <beyu94@gmail.com>
2019-08-03 11:22:36 -0700

    seed script complete, updated server routes, connected to postgres

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,52 +1,83 @@
 const faker = require('faker');
-const chalk = require('chalk');
-const db = require('../db');
+const fs = require('fs');
 
-const listings = [];
-const bookings = [];
+const listingStream = fs.createWriteStream('./listings.csv');
+const bookingStream = fs.createWriteStream('./bookings.csv');
+const userStream = fs.createWriteStream('./users.csv');
 
-for (let i = 0; i < 100; i += 1) {
-  const listingInfo = {
-    reviews: faker.random.number({ min: 0, max: 1000 }),
-    views: faker.random.number({ min: 0, max: 1000 }),
-    basePrice: faker.random.number({ min: 20, max: 500 }),
-    cleaningFee: faker.random.number({ min: 20, max: 500 }),
-    baseGuests: faker.random.number({ min: 1, max: 5 }) * 2,
-    minNights: faker.random.number({ min: 1, max: 10 }),
-  };
-  listingInfo.guestFee = faker.random.number({ min: 0, max: listingInfo.basePrice });
-  listingInfo.extraGuests = Math.round(
-    listingInfo.baseGuests * faker.random.number({ min: 0, max: 0.5, precision: 0.1 }),
-  );
-  listingInfo.maxGuests = listingInfo.baseGuests + listingInfo.extraGuests;
-  listingInfo.lastAvailableDate = faker.date.between('2019-09-01', '2020-05-31');
-  listingInfo.maxNights = Math.max(listingInfo.minNights,
-    faker.random.number({ min: 1, max: 355 }));
-  listingInfo.taxes = Math.round(
-    listingInfo.basePrice * faker.random.number({ min: 0.05, max: 0.2, precision: 0.01 }),
-  );
-  listingInfo.serviceFee = Math.round(
-    listingInfo.basePrice * 0.12,
-  );
+const maxListings = 1e7;
+const minUsers = 0;
+const maxUsers = 2; //max 2 user bookings on a listing
+// const minBookings = 50;
+// const maxBookings = 50;
+var userReserveCount = 0;
+var bookingCount = 0;
 
-  listings.push(listingInfo);
-
-  for (let j = 0; j < 200; j += 1) {
-    const bookingInfo = {
-      listingId: i + 1,
-      bookedDate: faker.date.between('2019-06-01', listingInfo.lastAvailableDate),
-    };
-    bookings.push(bookingInfo);
-  }
+function drainer() {
+  return new Promise((resolve) => {
+    listingStream.once('drain', resolve);
+  });
 }
+async function writer() {
+  let ableToWrite = true;
 
-Promise.all([
-  db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0'),
-  db.Booking.drop(),
-  db.Listing.drop(),
-  db.sequelize.sync({ force: false })
-    .then(() => db.Listing.bulkCreate(listings))
-    .then(() => db.Booking.bulkCreate(bookings))
-    .then(() => db.sequelize.connectionManager.close())
-    .then(() => console.log(chalk.green('shut down gracefully'))),
-]).catch(err => console.log('db error: ', err));
+  for (let i = 0; i < maxListings; i += 1) {//data for each listing
+    const listing_id = i + 1;
+    const reviews = faker.random.number({ min: 0, max: 1000 });
+    const views = faker.random.number({ min: 0, max: 1000 });
+    const basePrice = faker.random.number({ min: 20, max: 500 });
+    const cleaningFee = faker.random.number({ min: 20, max: 500 });  
+    const baseGuests = faker.random.number({ min: 1, max: 5 }) * 2;   
+    const minNights = faker.random.number({ min: 1, max: 10 });
+    const guestFee = faker.random.number({ min: 0, max: basePrice });
+    const extraGuests = Math.round(
+      baseGuests * faker.random.number({ min: 0, max: 0.5, precision: 0.1 })
+    );
+    const maxGuests = baseGuests + extraGuests;
+    const lastAvailableDate = faker.date.between('2019-09-01', '2019-12-10');
+    const maxNights = Math.max(minNights, faker.random.number({ min: 1, max: 355 }));
+    const taxes = Math.round(
+      basePrice * faker.random.number({ min: 0.05, max: 0.2, precision: 0.01 }),
+    );
+    const serviceFee = Math.round(
+      basePrice * 0.12,
+    );    
+    const randUsers = Math.floor(Math.random() * (maxUsers - minUsers + 1)) + minUsers; //The maximum is inclusive and the minimum is inclusive 
+    var startDate = faker.date.between('2019-07-31', lastAvailableDate);
+    for(let k = 0; k < randUsers; k++){//create users that contains start and end reservation date
+        userReserveCount +=1;
+        if(k===2){
+            startDate = faker.date.between(startDate, lastAvailableDate);
+        }
+        // const startDate = faker.date.between('2019-07-31', lastAvailableDate);
+        const endDate = faker.date.between(startDate, lastAvailableDate);
+        userStream.write(`${userReserveCount},${listing_id},${startDate.toISOString()},${endDate.toISOString()}\n`);
+        var date1 = new Date(startDate);
+        var date2 = new Date(endDate);
+        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        var currDate = date1;
+        
+        for (let j = 0; j < diffDays; j += 1) {//store days booked by user inside booking table
+            //const id = maxBookings*i + j;
+            bookingCount += 1;
+            bookingStream.write(`${bookingCount},${listing_id},${currDate.toISOString()},${userReserveCount}\n`);
+            currDate.setDate(currDate.getDate()+1);
+        }
+    }
+    
+    ableToWrite = listingStream.write(`${listing_id},${reviews},${views},${basePrice},${cleaningFee},${baseGuests},${minNights},${guestFee},${extraGuests},${maxGuests},${lastAvailableDate.toISOString()},${maxNights},${taxes},${serviceFee}\n`);
+
+    if (!ableToWrite) {
+      await drainer();
+    }
+    if (i % 1e6 === 0) {
+      console.error(i, 'count per mil');
+      console.error(process.uptime() + "sec");
+    }
+  }
+    bookingStream.end()
+    listingStream.end();
+    userStream.end();
+}
+writer();
